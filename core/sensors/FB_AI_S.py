a2625b647333d4713f8c16a5075b842e9a6eddbc
VladNuriev <82199938+VladNuriev@users.noreply.github.com>
2023-04-27 15:27:05 +0300

    Update FB_AI_S.py

--- core/sensors/FB_AI_S.py
+++ core/sensors/FB_AI_S.py
@@ -13,8 +13,6 @@ class FB_AI_S(Sensor):
     Name = Field(name='name', column='D', validators=[value_is_not_none_or_empty])
     EUnit = Field(name='EUnit', column='L', validators=[value_is_not_none_or_empty])
     ParName = Field(name='ParName', column='K', validators=[value_is_not_none_or_empty])
-    SensorType = Field(name='Sensor_Type', column='O', validators=[value_is_not_none_or_empty])
-    SensorPosition = Field(name='Sensor_Position', column='N')
     GP = Field(name='GeneralPlan', column='J', validators=[value_is_not_none_or_empty])
     Description = Field(name='Description', column='E', validators=[value_is_not_none_or_empty])
     IvxxTp = Field(name='IVXX_TP', column='Y')
@@ -25,8 +23,6 @@ class FB_AI_S(Sensor):
             f'      <attribute type="Attributes.{self.EUnit.name}" value="{getattr(self, self.EUnit.key)}"/>\n'
             f'      <attribute type="Attributes.FracDigits" value="2" />\n'
             f'      <attribute type="Attributes.{self.ParName.name}" value="{getattr(self, self.ParName.key)}"/>\n'
-            f'      <attribute type="Attributes.{self.SensorPosition.name}" value="{getattr(self, self.SensorPosition.key)}"/>\n'
-            f'      <attribute type="Attributes.{self.SensorType.name}" value="{getattr(self, self.SensorType.key)}"/>\n'
             f'      <attribute type="unit.System.Attributes.{self.Description.name}" value="{getattr(self, self.Description.key)}"/>\n'
             f'      <attribute type="Attributes.{self.IvxxTp.name}" value="{getattr(self, self.IvxxTp.key)}"/>\n'
             f'      <attribute type="Attributes.{self.GP.name}" value="{getattr(self, self.GP.key)}"/>\n'
