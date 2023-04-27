acf9c6798088d88a2999a2abdbddb901e9bb2efb
VladNuriev <82199938+VladNuriev@users.noreply.github.com>
2023-04-27 16:17:00 +0300

    Update FB_QSA_S.py

--- core/sensors/FB_QSA_S.py
+++ core/sensors/FB_QSA_S.py
@@ -12,11 +12,9 @@ class FB_QSA_S(Sensor):
     CLASS_NAME = 'QSA'
     Name = Field(name='name', column='D', validators=[value_is_not_none_or_empty])
     EUnit = Field(name='EUnit', column='L', validators=[value_is_not_none_or_empty])
-    SensorType = Field(name='Sensor_Type', column='O', validators=[value_is_not_none_or_empty])
-  # SensorPosition = Field(name='Sensor_Position', column='N')
     GP = Field(name='GeneralPlan', column='J', validators=[value_is_not_none_or_empty])
     Description = Field(name='Description', column='E', validators=[value_is_not_none_or_empty])
-    IvxxTp = Field(name='IVXX_TP', column='Y')
+    IvxxTp = Field(name='IVXX_TP', column='Y', validators=[value_is_not_none_or_empty])
     Substance = Field(name='Substance', column='N', validators=[value_is_not_none_or_empty])
     IfexTp = Field(name='IFEX_TP', column='AD')
     It1xTp = Field(name='IT1X_TP', column='Z')
@@ -27,8 +25,6 @@ class FB_QSA_S(Sensor):
             f'    <ct:object {self.Name.name}="{getattr(self, self.Name.key)}" base-type="{self.BASE_TYPE}" aspect="Aspects.PLC" access-level="public" uuid="{self.pk}">\n'
             f'      <attribute type="Attributes.{self.EUnit.name}" value="{getattr(self, self.EUnit.key)}"/>\n'
             f'      <attribute type="Attributes.FracDigits" value="2" />\n'
-          # f'      <attribute type="Attributes.{self.SensorPosition.name}" value="{getattr(self, self.SensorPosition.key)}"/>\n'
-            f'      <attribute type="Attributes.{self.SensorType.name}" value="{getattr(self, self.SensorType.key)}"/>\n'
             f'      <attribute type="unit.System.Attributes.{self.Description.name}" value="{getattr(self, self.Description.key)}"/>\n'
             f'      <attribute type="Attributes.{self.IvxxTp.name}" value="{getattr(self, self.IvxxTp.key)}"/>\n'
             f'      <attribute type="Attributes.{self.Substance.name}" value="{getattr(self, self.Substance.key)}"/>\n'
