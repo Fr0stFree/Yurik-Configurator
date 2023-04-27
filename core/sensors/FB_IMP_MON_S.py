186642092fd76e7892060c3eb5c640c8d4d5a0df
VladNuriev <82199938+VladNuriev@users.noreply.github.com>
2023-04-27 16:32:23 +0300

    Update FB_IMP_MON_S.py

--- core/sensors/FB_IMP_MON_S.py
+++ core/sensors/FB_IMP_MON_S.py
@@ -1,1 +1,26 @@
+from core.validators import value_is_not_none_or_empty
+from core.fields import Field, SeverityField
+from .sensor import Sensor
+
+class FB_IMp_MON_S(Sensor):
+    BASE_TYPE = 'Types.FB_IMp_MON_S.FB_IMp_MON_S_PLC'
+    CLASS_NAME = 'FB_IMp_MON_S'
+    Name = Field(name='name', column='D', validators=[value_is_not_none_or_empty])
+    Description = Field(name='Description', column='E', validators=[value_is_not_none_or_empty])
+    IfxxTp = Field(name='IFXX_TP', column='AD')
+    IofxTp = Field(name='IOFX_TP', column='AA')
+    IonxTp = Field(name='IONX_TP', column='Z')
+    GP = Field(name='GeneralPlan', column='J', validators=[value_is_not_none_or_empty])
+
+    def to_omx(self) -> str:
+        omx_block = (
+            f'    <ct:object {self.Name.name}="{getattr(self, self.Name.key)}" base-type="{self.BASE_TYPE}" aspect="Aspects.PLC" access-level="public" uuid="{self.pk}">\n'
+            f'      <attribute type="unit.System.Attributes.{self.Description.name}" value="{getattr(self, self.Description.key)}"/>\n'
+            f'      <attribute type="Attributes.{self.IfxxTp.name}" value="{getattr(self, self.IfxxTp.key)}"/>\n'
+            f'      <attribute type="Attributes.{self.IofxTp.name}" value="{getattr(self, self.IofxTp.key)}"/>\n'
+            f'      <attribute type="Attributes.{self.IonxTp.name}" value="{getattr(self, self.IonxTp.key)}"/>\n'
+            f'      <attribute type="Attributes.{self.GP.name}" value="{getattr(self, self.GP.key)}"/>\n'
+            f'    </ct:object>\n'
+        )
+        return omx_block
 
