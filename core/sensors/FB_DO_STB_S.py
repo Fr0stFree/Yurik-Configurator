7c4cb0de69c46c7403c781c5083fdd498fa79405
VladNuriev <82199938+VladNuriev@users.noreply.github.com>
2023-04-27 15:32:07 +0300

    Update FB_DO_STB_S.py

--- core/sensors/FB_DO_STB_S.py
+++ core/sensors/FB_DO_STB_S.py
@@ -11,11 +11,10 @@ class FB_DO_STB_S(Sensor):
     BASE_TYPE = 'Types.FB_DO_STB_S.FB_DO_STB_S_PLC'
     CLASS_NAME = 'DO'
     Name = Field(name='name', column='D', validators=[value_is_not_none_or_empty])
-    ColorOn = Field(name='ColorOn', column='Q')
+    ColorOn = Field(name='ColorOn', column='Q', validators=[value_is_not_none_or_empty])
     GP = Field(name='GeneralPlan', column='J', validators=[value_is_not_none_or_empty])
-    SoundOn = Field(name='SoundOn', column='P', validators=[value_is_not_none_or_empty])
     Description = Field(name='Description', column='E', validators=[value_is_not_none_or_empty])
-    Severity = SeverityField(name='SeverityOn', column='P')
+    SirenType = Field(name='SirenType', column='N', validators=[value_is_not_none_or_empty])
     OxonTp = Field(name='OXON_TP', column='AE')
 
     def to_omx(self) -> str:
@@ -23,9 +22,8 @@ class FB_DO_STB_S(Sensor):
             f'    <ct:object {self.Name.name}="{getattr(self, self.Name.key)}" base-type="{self.BASE_TYPE}" aspect="Aspects.PLC" access-level="public" uuid="{self.pk}">\n'
             f'      <attribute type="Attributes.{self.GP.name}" value="{getattr(self, self.GP.key)}"/>\n'
             f'      <attribute type="Attributes.{self.ColorOn.name}" value="{getattr(self, self.ColorOn.key)}"/>\n'
-            f'      <attribute type="Attributes.{self.SoundOn.name}" value="{getattr(self, self.SoundOn.key)}"/>\n'
             f'      <attribute type="unit.System.Attributes.{self.Description.name}" value="{getattr(self, self.Description.key)}"/>\n'
-            f'      <attribute type="Attributes.{self.Severity.name}" value="{getattr(self, self.Severity.key)}"/>\n'
+            f'      <attribute type="Attributes.{self.SirenType.name}" value="{getattr(self, self.SirenType.key)}"/>\n'
             f'      <attribute type="Attributes.{self.OxonTp.name}" value="{getattr(self, self.OxonTp.key)}"/>\n'
             f'    </ct:object>\n'
         )
