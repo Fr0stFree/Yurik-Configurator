a160f1a43933aa0ea618ff6d122075e91cd0018c
VladNuriev <82199938+VladNuriev@users.noreply.github.com>
2023-05-12 10:37:11 +0300

    Create M943DR.py

---
+++ core/sensors/M943DR.py
@@ -0,0 +1,22 @@
+from core.validators import value_is_not_none_or_empty
+from core.fields import Field, SeverityField
+from .sensor import Sensor
+
+
+class M943DR(Sensor):
+
+
+    BASE_TYPE = 'Types.M943DR.M943DR_PLC'
+    CLASS_NAME = 'DIAG'
+    Name = Field(name='name', column='D', validators=[value_is_not_none_or_empty])
+    Description = Field(name='Description', column='E', validators=[value_is_not_none_or_empty])
+    GP = Field(name='GeneralPlan', column='J')
+
+    def to_omx(self) -> str:
+        omx_block = (
+            f'    <ct:object {self.Name.name}="{getattr(self, self.Name.key)}" base-type="{self.BASE_TYPE}" aspect="Aspects.PLC" access-level="public" uuid="{self.pk}">\n'
+            f'      <attribute type="unit.System.Attributes.{self.Description.name}" value="{getattr(self, self.Description.key)}"/>\n'
+            f'      <attribute type="Attributes.{self.GP.name}" value="{getattr(self, self.GP.key)}"/>\n'
+            f'    </ct:object>\n'
+        )
+        return omx_block
