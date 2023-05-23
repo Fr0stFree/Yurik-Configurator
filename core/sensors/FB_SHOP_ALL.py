99b203e50ea466c91bb3aec17f80af2122307b36
VladNuriev <82199938+VladNuriev@users.noreply.github.com>
2023-05-23 09:01:52 +0300

    Update and rename FB_SHOP_ALL_S.py to FB_SHOP_ALL.py

--- core/sensors/FB_SHOP_ALL.py
+++ core/sensors/FB_SHOP_ALL.py
@@ -1,21 +1,13 @@
-# <ct:object name="SHOP_ALL" access-level="public" access-scope="global" uuid="fcc0348b-9bbe-4817-bfc0-d57e8442eb81">
-#     <attribute type="unit.Server.Attributes.NodeRelativePath" />
-#     <attribute type="unit.Server.Attributes.IsObject" value="false" />
-#     <ct:object name="GP001_OP" base-type="Types.FB_SHOP_ALL.FB_SHOP_ALL_PLC" access-level="public" access-scope="global" aspect="Aspects.PLC" uuid="8a1180bd-5ff7-43f2-a7db-128b59a22377">
-#       <attribute type="unit.System.Attributes.Description" value="ГП 1. Здание входных ниток и пробкоуловителей. Оповещение" />
-#       <attribute type="Attributes.GeneralPlan" value="ГП001" />
-#     </ct:object>
-#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!НОВЫЙ ДАТЧИК! ОБЯЗАТЕЛЬНО ВСЁ ПРОВЕРИТЬ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 from core.validators import value_is_not_none_or_empty
 from core.fields import Field, SeverityField
 from core.sensors import Sensor
 
-class FB_SHOP_ALL_S(Sensor):
+class FB_SHOP_ALL(Sensor):
     """
     Класс для работы с датчиками типа FB_DO_STB_S. Поле Severity отсутствует в таблице, его значение
     рассчитывается на основе значения в поле SOUND_ON.
     """
-    BASE_TYPE = 'Types.FB_SHOP_ALL.FB_SHOP_ALL_PLC'
+    BASE_TYPE = 'Types.SHOP_ALL.SHOP_ALL_PLC'
     CLASS_NAME = 'FB_SHOP_ALL'
     Name = Field(name='name', column='D', validators=[value_is_not_none_or_empty])
     Description = Field(name='Description', column='E', validators=[value_is_not_none_or_empty])
