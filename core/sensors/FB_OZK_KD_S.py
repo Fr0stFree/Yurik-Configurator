522ad15521949eb371e65bea8782953bb1b3e1ea
VladNuriev <82199938+VladNuriev@users.noreply.github.com>
2023-05-23 08:59:31 +0300

    Update FB_OZK_KD_S.py

--- core/sensors/FB_OZK_KD_S.py
+++ core/sensors/FB_OZK_KD_S.py
@@ -1,18 +1,3 @@
-# <ct:object name="OZK_KD" access-level="public" access-scope="global" uuid="9157ab67-0479-476b-8412-c058d1c32c4e">
-#     <attribute type="unit.Server.Attributes.NodeRelativePath" />
-#     <attribute type="unit.Server.Attributes.IsObject" value="false" />
-#     <ct:object name="GP001_OK_01" base-type="Types.FB_OZK_KD_S.FB_OZK_KD_S_PLC" access-level="public" access-scope="global" aspect="Aspects.PLC" uuid="42e27951-1915-4a81-8d12-5ba7483d4183">
-#       <attribute type="unit.System.Attributes.Description" value="ГП 1. ОЗК ОК1 системы П1" />
-#       <attribute type="Attributes.IFXX_TP" value="-" />
-#       <attribute type="Attributes.IOFX_TP" value="-" />
-#       <attribute type="Attributes.IONX_TP" value="-" />
-#       <attribute type="Attributes.OXOF_TP" value="-" />
-#       <attribute type="Attributes.OXON_TP" value="-" />
-#       <attribute type="Attributes.GeneralPlan" value="ГП001" />
-#       <attribute type="Attributes.ISOF_TP" value="-" />
-#       <attribute type="Attributes.ISON_TP" value="-" />
-#     </ct:object>
-#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!НОВЫЙ ДАТЧИК! ОБЯЗАТЕЛЬНО ВСЁ ПРОВЕРИТЬ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 from core.validators import value_is_not_none_or_empty
 from core.fields import Field, SeverityField
 from .sensor import Sensor
@@ -21,7 +6,7 @@ class FB_OZK_KD_S(Sensor):
     Класс для работы с датчиками типа FB_OZK_KD_S. Поле Severity отсутствует в таблице, его значение
     рассчитывается на основе значения в поле SOUND_ON.
     """
-    BASE_TYPE = 'Types.FB_OZK_KD_S.FB_OZK_KD_S_PLC'
+    BASE_TYPE = 'Types.OZK_KD.OZK_KD_PLC'
     CLASS_NAME = 'FB_OZK_KD_S'
     Name = Field(name='name', column='D', validators=[value_is_not_none_or_empty])
     Description = Field(name='Description', column='E', validators=[value_is_not_none_or_empty])
@@ -32,7 +17,8 @@ class FB_OZK_KD_S(Sensor):
     OxonTp = Field(name='OXON_TP', column='AE')
     GP = Field(name='GeneralPlan', column='J', validators=[value_is_not_none_or_empty])
     OsofTp = Field(name='ISOF_TP', column='AJ')
-    IsonTp = Field(name='ISON_TP', column='AI') 
+    IsonTp = Field(name='ISON_TP', column='AI')
+
 
     def to_omx(self) -> str:
         omx_block = (
@@ -49,3 +35,4 @@ class FB_OZK_KD_S(Sensor):
             f'    </ct:object>\n'
         )
         return omx_block
+
