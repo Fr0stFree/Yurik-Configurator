6fce3bdcb93ad96433cbac8afe040b824c1b14a6
VladNuriev <82199938+VladNuriev@users.noreply.github.com>
2023-05-23 09:47:21 +0300

    Update fields.py

--- core/fields.py
+++ core/fields.py
@@ -10,17 +10,17 @@ class Field(NamedTuple):
     name: str
     column: Optional[str]
     validators: Optional[list] = None
-    
+
     @property
     def key(self) -> str:
         return to_snake_case(self.name)
-    
+
     def _validate(self, value) -> None:
         if not self.validators:
             return
         for validator in self.validators:
             validator(value)
-    
+
     def get_value(self, sheet: Worksheet, row: int) -> str:
         value = sheet[f'{self.column}{row}'].value
         try:
@@ -37,7 +37,10 @@ class SeverityField(Field):
                   "Порог 1": 6, "Предупреждение": 8, "Недостоверность": 10,
                   "Неисправность": 12, "Отключение": 13, "Ремонт": 14, "Имитация": 16,
                   "Телесигнализация": 18, "Команда оператора": 20, "Информация": 22}
-    
+
+class MessageField(Field):
+    severities = {"Пожар": 'Пожар', "Тревога": 'Сработка'}
+
     def get_value(self, sheet: Worksheet, row: int) -> str:
         value = super().get_value(sheet, row)
         if value not in self.severities:
