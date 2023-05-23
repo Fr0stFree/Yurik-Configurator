e7166904d99e2b4b3a72eb1994f42156a3089d7e
VladNuriev <82199938+VladNuriev@users.noreply.github.com>
2023-05-23 09:43:29 +0300

    Update SW_MES3508.py

--- core/sensors/SW_MES3508.py
+++ core/sensors/SW_MES3508.py
@@ -3,39 +3,41 @@ from core.fields import Field, SeverityField
 from core.sensors import Sensor
 
 class SW_MES3508(Sensor):
-    BASE_TYPE = 'Types.SW_MES3508.SW_MES3508_PLC'
+    BASE_TYPE = 'Types.DIAG.SW_MES3508.SW_MES3508_PLC'
     CLASS_NAME = 'MES3508'
     Name = Field(name='name', column='D', validators=[value_is_not_none_or_empty])
     Description = Field(name='Description', column='E', validators=[value_is_not_none_or_empty])
     GP = Field(name='GeneralPlan', column='J', validators=[value_is_not_none_or_empty])
-    IvxxTp = Field(name='IVXX_TP', column='Y')
-    IffxTp = Field(name='IFEX_TP', column='AD')
-    OxonTp = Field(name='OXON_TP', column='AE')
-    IofxTp = Field(name='IOFX_TP', column='AA')
-    IonxTp = Field(name='IONX_TP', column='Z')
-    OrcxTp = Field(name='IRCX_TP', column='AC')
-    OxofTp = Field(name='OXOF_TP', column='AF')
-    IlcxTp = Field(name='ILCX_TP', column='AB')
-    OxspTp = Field(name='OXSP_TP', column='AG')
-    OsofTp = Field(name='ISOF_TP', column='AJ')
-    IsonTp = Field(name='ISON_TP', column='AI')
-    ItonTp = Field(name='ITON_TP', column='AH')
+    AbonentPort1 = Field(name='AbonentPort1', column='Y')
+    AbonentPort2 = Field(name='AbonentPort2', column='AD')
+    AbonentPort3 = Field(name='AbonentPort3', column='AE')
+    AbonentPort4 = Field(name='AbonentPort4', column='AA')
+    AbonentPort5 = Field(name='AbonentPort5', column='Z')
+    AbonentPort6 = Field(name='AbonentPort6', column='AC')
+    AbonentPort7 = Field(name='AbonentPort7', column='AF')
+    AbonentPort8 = Field(name='AbonentPort8', column='AB')
+    AbonentPort9 = Field(name='AbonentPort9', column='AG')
+    AbonentPort10 = Field(name='Abonent_Port10', column='AJ')
+    AbonentPort11 = Field(name='AbonentPort11', column='AI')
+    AbonentPort12 = Field(name='AbonentPort12', column='AH')
 
     def to_omx(self) -> str:
         omx_block = (
             f'    <ct:object {self.Name.name}="{getattr(self, self.Name.key)}" base-type="{self.BASE_TYPE}" aspect="Aspects.PLC" access-level="public" uuid="{self.pk}">\n'
             f'      <attribute type="unit.System.Attributes.{self.Description.name}" value="{getattr(self, self.Description.key)}"/>\n'
-            f'      <attribute type="Attributes.{self.IffxTp.name}" value="{getattr(self, self.IffxTp.key)}"/>\n'
-            f'      <attribute type="Attributes.{self.IofxTp.name}" value="{getattr(self, self.IofxTp.key)}"/>\n'
-            f'      <attribute type="Attributes.{self.IonxTp.name}" value="{getattr(self, self.IonxTp.key)}"/>\n'
-            f'      <attribute type="Attributes.{self.OrcxTp.name}" value="{getattr(self, self.OrcxTp.key)}"/>\n'
-            f'      <attribute type="Attributes.{self.OxofTp.name}" value="{getattr(self, self.OxofTp.key)}"/>\n'
-            f'      <attribute type="Attributes.{self.OxonTp.name}" value="{getattr(self, self.OxonTp.key)}"/>\n'
-            f'      <attribute type="Attributes.{self.IlcxTp.name}" value="{getattr(self, self.IlcxTp.key)}"/>\n'
-            f'      <attribute type="Attributes.{self.OxspTp.name}" value="{getattr(self, self.OxspTp.key)}"/>\n'
+            f'      <attribute type="Attributes.{self.AbonentPort1.name}" value="{getattr(self, self.AbonentPort1.key)}"/>\n'
+            f'      <attribute type="Attributes.{self.AbonentPort2.name}" value="{getattr(self, self.AbonentPort2.key)}"/>\n'
+            f'      <attribute type="Attributes.{self.AbonentPort3.name}" value="{getattr(self, self.AbonentPort3.key)}"/>\n'
+            f'      <attribute type="Attributes.{self.AbonentPort4.name}" value="{getattr(self, self.AbonentPort4.key)}"/>\n'
+            f'      <attribute type="Attributes.{self.AbonentPort5.name}" value="{getattr(self, self.AbonentPort5.key)}"/>\n'
+            f'      <attribute type="Attributes.{self.AbonentPort6.name}" value="{getattr(self, self.AbonentPort6.key)}"/>\n'
+            f'      <attribute type="Attributes.{self.AbonentPort7.name}" value="{getattr(self, self.AbonentPort7.key)}"/>\n'
+            f'      <attribute type="Attributes.{self.AbonentPort8.name}" value="{getattr(self, self.AbonentPort8.key)}"/>\n'
             f'      <attribute type="Attributes.{self.GP.name}" value="{getattr(self, self.GP.key)}"/>\n'
-            f'      <attribute type="Attributes.{self.IsonTp.name}" value="{getattr(self, self.IsonTp.key)}"/>\n'
-            f'      <attribute type="Attributes.{self.ItonTp.name}" value="{getattr(self, self.ItonTp.key)}"/>\n'
+            f'      <attribute type="Attributes.{self.AbonentPort9.name}" value="{getattr(self, self.AbonentPort9.key)}"/>\n'
+            f'      <attribute type="Attributes.{self.AbonentPort10.name}" value="{getattr(self, self.AbonentPort10.key)}"/>\n'
+            f'      <attribute type="Attributes.{self.AbonentPort11.name}" value="{getattr(self, self.AbonentPort11.key)}"/>\n'
+            f'      <attribute type="Attributes.{self.AbonentPort12.name}" value="{getattr(self, self.AbonentPort12.key)}"/>\n'
             f'    </ct:object>\n'
         )
         return omx_block
