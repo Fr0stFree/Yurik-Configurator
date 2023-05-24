dca6af25e1f39cf27b7642d9a94d0ce788ebda80
VladNuriev <82199938+VladNuriev@users.noreply.github.com>
2023-05-24 08:54:04 +0300

    Update M541HR.py

--- core/sensors/M541HR.py
+++ core/sensors/M541HR.py
@@ -7,7 +7,7 @@ class M541HR(Sensor):
 
 
     BASE_TYPE = 'Types.DIAG.M541HR.M541HR_PLC'
-    CLASS_NAME = 'DIAG'
+    CLASS_NAME = 'M41HR'
     Name = Field(name='name', column='D', validators=[value_is_not_none_or_empty])
     Description = Field(name='Description', column='E', validators=[value_is_not_none_or_empty])
     GP = Field(name='GeneralPlan', column='J', validators=[value_is_not_none_or_empty])
