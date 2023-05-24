36669c04cf42062757c8cfd721320325b882f6d9
VladNuriev <82199938+VladNuriev@users.noreply.github.com>
2023-05-24 08:57:43 +0300

    Update M915E.py

--- core/sensors/M915E.py
+++ core/sensors/M915E.py
@@ -7,7 +7,7 @@ class M915E(Sensor):
 
 
     BASE_TYPE = 'Types.DIAG.M915E.M915E_PLC'
-    CLASS_NAME = 'DIAG'
+    CLASS_NAME = 'M915E'
     Name = Field(name='name', column='D', validators=[value_is_not_none_or_empty])
     Description = Field(name='Description', column='E', validators=[value_is_not_none_or_empty])
     GP = Field(name='GeneralPlan', column='J', validators=[value_is_not_none_or_empty])
