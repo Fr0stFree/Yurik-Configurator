e65f13519c344e02aa2d71b8e3625efd39662c2b
VladNuriev <82199938+VladNuriev@users.noreply.github.com>
2023-05-24 09:01:13 +0300

    Update M991S.py

--- core/sensors/M991S.py
+++ core/sensors/M991S.py
@@ -7,7 +7,7 @@ class M991S(Sensor):
 
 
     BASE_TYPE = 'Types.DIAG.M991S.M991S_PLC'
-    CLASS_NAME = 'DIAG'
+    CLASS_NAME = 'M991S'
     Name = Field(name='name', column='D', validators=[value_is_not_none_or_empty])
     Description = Field(name='Description', column='E', validators=[value_is_not_none_or_empty])
     GP = Field(name='GeneralPlan', column='J', validators=[value_is_not_none_or_empty])
