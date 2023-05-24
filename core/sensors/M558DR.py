728f1508f5736fe979e471444f90bf5dd7e7ca2c
VladNuriev <82199938+VladNuriev@users.noreply.github.com>
2023-05-24 08:56:59 +0300

    Update M558DR.py

--- core/sensors/M558DR.py
+++ core/sensors/M558DR.py
@@ -7,7 +7,7 @@ class M558DR(Sensor):
 
 
     BASE_TYPE = 'Types.DIAG.M558DR.M558DR_PLC'
-    CLASS_NAME = 'DIAG'
+    CLASS_NAME = 'M558DR'
     Name = Field(name='name', column='D', validators=[value_is_not_none_or_empty])
     Description = Field(name='Description', column='E', validators=[value_is_not_none_or_empty])
     GP = Field(name='GeneralPlan', column='J', validators=[value_is_not_none_or_empty])
