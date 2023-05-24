02650e270092a47e8b7c670b5fbd87ae94387c56
VladNuriev <82199938+VladNuriev@users.noreply.github.com>
2023-05-24 09:00:18 +0300

    Update M957D.py

--- core/sensors/M957D.py
+++ core/sensors/M957D.py
@@ -7,7 +7,7 @@ class M957D(Sensor):
 
 
     BASE_TYPE = 'Types.DIAG.M957D.M957D_PLC'
-    CLASS_NAME = 'DIAG'
+    CLASS_NAME = 'M957D'
     Name = Field(name='name', column='D', validators=[value_is_not_none_or_empty])
     Description = Field(name='Description', column='E', validators=[value_is_not_none_or_empty])
     GP = Field(name='GeneralPlan', column='J', validators=[value_is_not_none_or_empty])
