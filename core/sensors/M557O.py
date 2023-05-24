5d75910721834267c0b3b69aed2a91d8817d289f
VladNuriev <82199938+VladNuriev@users.noreply.github.com>
2023-05-24 08:56:19 +0300

    Update M557O.py

--- core/sensors/M557O.py
+++ core/sensors/M557O.py
@@ -7,7 +7,7 @@ class M557O(Sensor):
 
 
     BASE_TYPE = 'Types.DIAG.M557O.M557O_PLC'
-    CLASS_NAME = 'DIAG'
+    CLASS_NAME = 'M557O'
     Name = Field(name='name', column='D', validators=[value_is_not_none_or_empty])
     Description = Field(name='Description', column='E', validators=[value_is_not_none_or_empty])
     GP = Field(name='GeneralPlan', column='J', validators=[value_is_not_none_or_empty])
